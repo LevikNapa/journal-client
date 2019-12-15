069a70a924b13c6076c1670ef4428dcd63dfd12f
github username <github email address>
2019-12-15 11:58:24 -0500

    added add entry action to reducer file

--- src/reducers/entryReducer.js
+++ src/reducers/entryReducer.js
@@ -2,7 +2,8 @@ export default function entryReducer(state = {entries:[]}, action) {
   switch (action.type) {
     case 'FETCH_ENTRIES':
       return {entries: action.payload}
-
+    case 'ADD_ENTRY':
+    return {...state, entries: [...state.entries, action.payload]}
     default:
       return state
   }
