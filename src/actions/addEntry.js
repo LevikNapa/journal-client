069aaca0170c744434bef3caf0e49919feced1dc
github username <github email address>
2019-12-15 11:58:56 -0500

    added action creator for adding entries to DB

---
+++ src/actions/addEntry.js
@@ -0,0 +1,17 @@
+export const addEntry = (data) => {
+
+   return (dispatch) => {
+     fetch('http://localhost:3000/entries', {
+       headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json'
+       },
+       method: 'POST',
+       body:JSON.stringify(data)
+     })
+     .then(response => response.json())
+     .then(data => dispatch({type: 'ADD_ENTRY', payload: data}))
+
+
+   }
+}
